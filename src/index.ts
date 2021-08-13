4e6729eb1bb46c17451c724d43e9bb2564340659
sharad-s <sharadshekar@gmail.com>
2021-08-12 23:29:55 -0500

    letsa fucking go

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+// @ts-nocheck
 import { add, subtract } from './math';
 import { toLowerCase, toUpperCase } from './string';
 
-export { add, subtract, toLowerCase, toUpperCase };
+const sayCheese = () => "cheese"
+const sayPoop = () => "poop"
+
+export { add, subtract, toLowerCase, toUpperCase, sayCheese, sayPoop };
